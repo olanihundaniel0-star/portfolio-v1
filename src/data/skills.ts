eab3f91e473c8eda1c007a35d00e4fe8b5189da4
olanihundaniel0-star <olanihundaniel0@gmail.com>
2026-05-10 18:20:20 +0100

    feat: updated tech stack

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -5,12 +5,9 @@ export interface Skill {
 }
 
 export const SKILLS: Skill[] = [
-  { name: 'Python', type: 'Language', level: 70 },
-  { name: 'React', type: 'Framework', level: 72 },
-  { name: 'TypeScript', type: 'Language', level: 75 },
-  { name: 'NestJS', type: 'Framework', level: 50 },
-  { name: 'Tailwind', type: 'CSS Framework', level: 88 },
   { name: 'JavaScript', type: 'Language', level: 80 },
-  { name: 'HTML / CSS', type: 'Markup & Style', level: 100 },
-  { name: 'C', type: 'Language', level: 30 },
+  { name: 'TypeScript', type: 'Language', level: 75 },
+  { name: 'HTML', type: 'Markup', level: 90 },
+  { name: 'CSS', type: 'Style', level: 90 },
+  { name: 'Python', type: 'Language', level: 70 },
 ];
